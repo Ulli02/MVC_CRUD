7390a360e381c13c49c7ce4ae15cd1ed945d75b9
sagarulli <sagarulli5@gmail.com>
2024-12-10 17:40:09 +0530

    MVC

---
+++ src/main/java/mvc/dto/Employee.java
@@ -0,0 +1,64 @@
+package mvc.dto;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int Id;
+	private String name;
+	private String email;
+	private long mobile;
+	private double salary;
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [Id=" + Id + ", name=" + name + ", email=" + email + ", mobile=" + mobile + ", salary="
+				+ salary + "]";
+	}
+
+}
